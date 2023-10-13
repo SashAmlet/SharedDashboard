f516a65fa1d73b038f325b086f1b63cbc38d00b9
Sasha Ostrenko <ostrenkosasha@gmail.com>
2023-10-13 16:46:51 +0300

    added the ability to parallelize users across rooms (across servers)

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,27 @@
 var socket;
+const globalServerPort = 3000;
+var inputString = '';
 
 function setup(){
     createCanvas(600, 400);
     background(51);
 
+
+    // Adding an input field
+    var input = createInput();
+    input.position(10, height + 10);
+    var button = createButton('Connect');
+    button.position(input.x + input.width, height + 10);
+
     socket = io.connect('http://localhost:3000'); //join the server, and at the same time send a message "connection"
+
+    button.mousePressed(function() {
+        var subServerName = input.value();
+        inputString = subServerName;
+        // Joining the selected subserver (room)
+        socket.emit('joinSubServer', subServerName);
+    });
+
     socket.on('mouse', newDrawing); // if there is message 'mouse', from the server side (server.js), call newDrawing func 
 
 }
@@ -13,21 +30,22 @@ function newDrawing(data){
     // this means that someone is currently drawing, so we need to update the dashboard
     noStroke();
     fill(255);
-    ellipse(data.x, data.y, 36, 36);
+    ellipse(data.x, data.y, 20, 20);
 
 }
 
 function mouseDragged(){
     var data = {
         x: mouseX,
-        y: mouseY
+        y: mouseY,
+        subServerName: inputString
     }
     socket.emit('mouse', data); // every time you draw something, a message is sent to the server with the keyword "mouse" and the data "data".
     console.log('Sending: ' + data);
 
     noStroke();
     fill(255);
-    ellipse(mouseX, mouseY, 36, 36);
+    ellipse(mouseX, mouseY, 20, 20);
 }
 
 function draw(){
--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 var express = require('express');
-
 var app = express();
-var server = app.listen(3000); // port: 3000
+
+// Port for root server
+var rootServerPort = 3000;
+var rootServer = app.listen(rootServerPort);
 
 app.use(express.static('public')); // as if we are saying that everything in the "public" folder will be client side
 
-console.log("My socket server is running");
-
-
+console.log("Global socket server is running on port " + rootServerPort);
 
 
 var socket = require('socket.io');
 
-var io = socket(server);
+var io = socket(rootServer);
 
 io.sockets.on('connection', newConnection); // if there is message 'connection', from the client side (sketch.js), call newConnection func
 
 function newConnection(socket){
     console.log('new connection: ' + socket.id);
 
+    socket.on('joinSubServer', (subServerName) => {
+        // Checking the existence of a room with the same name
+        if (!io.sockets.adapter.rooms.has(subServerName)) {
+            // Creating a new room if it doesn't exist
+            socket.join(subServerName);
+            console.log('Subserver "' + subServerName + '" is created.');
+        } else {
+            // Joining an existing room
+            socket.join(subServerName);
+        }
+    });
+
     socket.on('mouse', mouseMsg); // if there is message 'mouse', from the client side (sketch.js), call mouseMsg func
 
     function mouseMsg(data){
         // this means that someone is currently drawing, so we need to send information to all other clients to update the dashboard.
         console.log(data);
-        socket.broadcast.emit('mouse', data);
+        // Forward drawing data to all users in the room
+        io.to(data.subServerName).emit('mouse', data);
+        // socket.broadcast.emit('mouse', data);
         // io.sockets.emit('mouse', data)
     }
 }
